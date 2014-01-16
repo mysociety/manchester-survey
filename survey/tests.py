89fcf4aef304c321b3978736b3913cca85a7535a
Struan Donald <struan@exo.org.uk>
2014-01-16 18:16:35 +0000

    check that cannot complete survey twice

--- survey/tests.py
+++ survey/tests.py
@@ -7,3 +7,15 @@ class StartPageTest(TestCase):
         response = self.client.get(reverse('survey:survey'))
         self.assertEqual(response.status_code, 200)
         self.assertContains(response, "University of Manchester")
+
+    def test_cannot_complete_survey_twice(self):
+        response = self.client.get(reverse('survey:survey'))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "University of Manchester")
+
+        self.client.post(reverse('survey:record'))
+        self.assertIsNotNone(self.client.cookies['usercode'])
+
+        response = self.client.get(reverse('survey:survey'))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "already completed")
