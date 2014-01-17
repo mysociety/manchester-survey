72103059f2c83234961b8831fe84e317212e9d55
Struan Donald <struan@exo.org.uk>
2014-01-17 11:22:38 +0000

    test to check that creating a survey creates a user

--- survey/tests.py
+++ survey/tests.py
@@ -1,6 +1,7 @@
 from django.core.urlresolvers import reverse
 from django.test import TestCase
 
+from survey.models import User
 
 class StartPageTest(TestCase):
     def test_front_page_displays(self):
@@ -19,3 +20,15 @@ class StartPageTest(TestCase):
         response = self.client.get(reverse('survey:survey'))
         self.assertEqual(response.status_code, 200)
         self.assertContains(response, "already completed")
+
+    def test_completing_survey_creates_user(self):
+        response = self.client.get(reverse('survey:survey'))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "University of Manchester")
+
+        self.client.post(reverse('survey:record'))
+        self.assertIsNotNone(self.client.cookies['usercode'].value)
+
+        usercode = self.client.cookies['usercode']
+        u = User.objects.get(code=usercode.value)
+        self.assertIsNotNone(u.id)
