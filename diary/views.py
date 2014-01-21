6535fd1afb24ec87a6bc5dd9bc0bc30ed959fd18
Struan Donald <struan@exo.org.uk>
2014-01-21 15:05:58 +0000

    basic save diary registration and send email

--- diary/views.py
+++ diary/views.py
@@ -1,5 +1,8 @@
+from datetime import date
+
 from django.shortcuts import render_to_response
 from django.template import RequestContext
+from django.core.mail import send_mail
 
 from diary.forms import RegisterForm
 from survey.models import User
@@ -8,6 +11,10 @@ def register(request):
     if request.method == 'POST':
         form = RegisterForm(request.POST)
         if form.is_valid():
+            u = User.objects.get(code=request.session['u'])
+            u.startdate = date.today()
+            u.save()
+            send_start_email(u)
             return render_to_response('register_thanks.html', {}, context_instance=RequestContext(request))
         else:
             return render_to_response('register.html', { 'form': form }, context_instance=RequestContext(request))
@@ -15,11 +22,20 @@ def register(request):
         try:
             token = request.GET['t']
             u = User.objects.get(token=token)
+            request.session['u'] = u.code
         except:
             return render_to_response('invalid_registration.html', {}, context_instance=RequestContext(request))
+
+        if u.startdate:
+            return render_to_response('already_registered.html', {}, context_instance=RequestContext(request))
 
         form = RegisterForm()
         return render_to_response('register.html', { 'form': form }, context_instance=RequestContext(request))
 
 def participant_info(request):
     return render_to_response('participant_info.html', {}, context_instance=RequestContext(request))
+
+def send_start_email(user):
+    #TODO: template this
+    send_mail('Initial Diary Entry', 'This is link for the diary for the first week', 'from@example.com',
+    [user.email])
