f0819058f0df3622a76248f5dfd31651d3c103ea
Struan Donald <struan@exo.org.uk>
2014-01-29 16:40:40 +0000

    implement question exclusion logic in javascript

---
+++ web/javascripts/app.js
@@ -0,0 +1,36 @@
+(function($) {
+    $('#a1f').on('click', function() {
+        if ( $('#a1f').prop('checked') ) {
+            $('input[name="1"]').prop('disabled', 1);
+            $('input[name="a1other"]').prop('disabled', 1);
+            $('#a1f').prop('disabled', 0);
+        } else {
+            $('input[name="a1other"]').prop('disabled', 0);
+            $('input[name="1"]').prop('disabled', 0);
+        }
+    });
+
+    $('#a12c').on('click', function() {
+        if ( $('#a12c').prop('checked') ) {
+            $('input[name="12"]').prop('disabled', 1);
+            $('#a12c').prop('disabled', 0);
+        } else {
+            $('input[name="12"]').prop('disabled', 0);
+        }
+    });
+
+    function toggle_fourteen_optional() {
+        if ( $(this).prop('checked') ) {
+            console.log('checked');
+            $('#screen14-2').show();
+        } else {
+            console.log('not checked');
+            $('#screen14-2').hide();
+        }
+    }
+
+    $('input[name="14community"][value="f"]').on('click', toggle_fourteen_optional);
+    $('input[name="14political"][value="f"]').on('click', toggle_fourteen_optional);
+
+    $('#screen14-2').hide();
+})(Zepto);
