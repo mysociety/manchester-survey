3f5b611e3075b1371f62bd745bb2ccffc1ed9b1a
Struan Donald <struan@exo.org.uk>
2014-02-06 17:12:21 +0000

    email text and formatting tweaks

--- diary/models.py
+++ diary/models.py
@@ -37,7 +37,7 @@ class ReminderManager(models.Manager):
 
         users = User.objects.filter(startdate__isnull=True).exclude(email__isnull=True)
 
-        self.send_email('email/registration_confirm.txt', 'Diary Registration', 'from@example.org', users)
+        self.send_email('email/registration_confirm.txt', 'Participate in the mySociety diary!', settings.FROM_EMAIL, users)
 
     def send_first_reminder_email(self):
         sd = SurveyDate()
@@ -50,7 +50,7 @@ class ReminderManager(models.Manager):
 
         users = User.objects.filter(startdate__gte=twelve_weeks_ago).filter(withdrawn=False)
 
-        self.send_email('email/first_reminder.txt', 'This week\'s diary', settings.FROM_EMAIL, users)
+        self.send_email('email/first_reminder.txt', 'This week\'s mySociety diary', settings.FROM_EMAIL, users)
 
     def send_second_reminder_email(self):
         sd = SurveyDate()
@@ -70,7 +70,7 @@ class ReminderManager(models.Manager):
 
             users = User.objects.filter(withdrawn=False).filter(startdate__lte=end_date).filter(startdate__gte=start_date).exclude(entries__week_id=week_id)
 
-            self.send_email('email/second_reminder.txt', 'This week\'s diary', settings.FROM_EMAIL, users)
+            self.send_email('email/second_reminder.txt', 'This week\'s mySociety diary', settings.FROM_EMAIL, users)
 
             #print 'users for week %d: %d' % ( week_num + 1, users.count() )
             #print 'start: %s, end: %s' % ( start_date, end_date )
