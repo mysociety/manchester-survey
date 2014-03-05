709f7fa2e93876a825db7e841dd64e8fdae8cb45
Struan Donald <struan@exo.org.uk>
2014-03-05 18:52:07 +0000

    make various urls case insensitive
    there is the odd 404 for these

--- diary/urls.py
+++ diary/urls.py
@@ -3,8 +3,8 @@ from django.conf.urls import patterns, url
 from diary.views import *
 
 urlpatterns = patterns('',
-    url(r'^R/(?P<id>[0-9A-Za-z]+)-(?P<token>.+)/$', 'diary.views.register', name='register'),
-    url(r'^D/(?P<id>[0-9A-Za-z]+)-(?P<token>.+)/$', 'diary.views.questions_for_week', name='questions'),
+    url(r'^[Rr]/(?P<id>[0-9A-Za-z]+)-(?P<token>.+)/$', 'diary.views.register', name='register'),
+    url(r'^[Dd]/(?P<id>[0-9A-Za-z]+)-(?P<token>.+)/$', 'diary.views.questions_for_week', name='questions'),
     url(r'^CW/(?P<id>[0-9A-Za-z]+)-(?P<token>.+)/$', 'diary.views.confirm_withdraw', name='confirm_withdraw'),
     url(r'^W/(?P<id>[0-9A-Za-z]+)-(?P<token>.+)/$', 'diary.views.withdraw', name='withdraw'),
     url(r'^record_diary', 'diary.views.record_answers', name='record_answers'),
