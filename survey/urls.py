b657ebeeae56ec109a61b19e442229ac84d78ca3
Struan Donald <struan@exo.org.uk>
2014-07-14 15:47:14 +0100

    survey closing instructions comment added

--- survey/urls.py
+++ survey/urls.py
@@ -6,6 +6,7 @@ urlpatterns = patterns('',
     url(r'^about', 'survey.views.about', name='about'),
     url(r'^management', 'survey.views.management', name='management'),
     url(r'^contact', 'survey.views.contact', name='contact'),
+    # comment out line below to close survey
     url(r'^survey2/(?P<id>[0-9A-Za-z]+)-(?P<token>.+)/$', 'survey.views.survey2', name='survey2'),
     url(r'^[Ss]/.*$', 'survey.views.closed', name='survey'),
     url(r'^record$', 'survey.views.record', name='record'),
