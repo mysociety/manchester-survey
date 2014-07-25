8c92c76d11b297e0b68b5f1b1711f462064fb33e
Chris Mytton <self@hecticjeff.net>
2014-07-25 17:53:44 +0100

    Remove survey2 route to close the survey
    Close the survey for the Manchester researchers.

--- survey/urls.py
+++ survey/urls.py
@@ -7,7 +7,7 @@ urlpatterns = patterns('',
     url(r'^management', 'survey.views.management', name='management'),
     url(r'^contact', 'survey.views.contact', name='contact'),
     # comment out line below to close survey
-    url(r'^survey2/(?P<id>[0-9A-Za-z]+)-(?P<token>.+)/$', 'survey.views.survey2', name='survey2'),
+    # url(r'^survey2/(?P<id>[0-9A-Za-z]+)-(?P<token>.+)/$', 'survey.views.survey2', name='survey2'),
     url(r'^[Ss]/.*$', 'survey.views.closed', name='survey'),
     url(r'^record$', 'survey.views.record', name='record'),
     url(r'^record2$', 'survey.views.record2', name='record2'),
